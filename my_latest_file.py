69abd99952740bcf2169a2a44a4b606c9d03ec82
Code10x-letscodewithManju <manjusuresh197490@gmail.com>
2026-04-19 14:17:06 +0530

    feat: new file created from the vscode terminal

--- my_latest_file.py
+++
@@ -1,3 +0,0 @@
-#this is the sample python file to test the latest file functionality
-print("This is the latest file")
-print("This file is used to test the latest file functionality in the system")
